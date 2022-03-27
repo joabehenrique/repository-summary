b45698b41ca6b24976d074dcc27025535e9e8d5b
joabehenrique <henriquecidoz@hotmail.com>
2022-03-26 21:11:21 -0300

    feat: added delegate of create_user

--- repository_summary/lib/repository_summary.ex
+++ repository_summary/lib/repository_summary.ex
@@ -1,5 +1,7 @@
 defmodule RepositorySummary do
   alias RepositorySummary.FindRepository, as: FindRepository
+  alias RepositorySummary.User.Create, as: CreateUser
 
   defdelegate find_repositorys(params), to: FindRepository, as: :call
+  defdelegate create_user(params), to: CreateUser, as: :call
 end
