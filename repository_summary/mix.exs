63afd064beee098a583221d50b5d43741b3cb148
joabehenrique <henriquecidoz@hotmail.com>
2022-03-26 21:10:26 -0300

    build: added pbkdf2 and guardian dependency

--- repository_summary/mix.exs
+++ repository_summary/mix.exs
@@ -46,7 +46,9 @@ defmodule RepositorySummary.MixProject do
       {:tesla, "~> 1.4"},
       {:hackney, "~> 1.17"},
       {:bypass, "~> 2.1", only: :test},
-      {:ex_machina, "~> 2.7.0"}
+      {:ex_machina, "~> 2.7.0"},
+      {:pbkdf2_elixir, "~> 1.3"},
+      {:guardian, "~> 2.0"}
     ]
   end
 
